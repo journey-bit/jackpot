9d8aefec1eac30e54bd6a91ed8ee687cb412ebec
lucky <lucky@luckydeMBP.lan>
2020-02-04 00:21:18 +0800

    init

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from flaskr import create_app
+import flaskr
 
-http_server = HTTPServer(WSGIContainer(create_app))
+http_server = HTTPServer(WSGIContainer(flaskr.create_app()))
 http_server.listen(8888)
 http_server.start(0)
 IOLoop.instance().start()
